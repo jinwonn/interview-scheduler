3cdebef51b5213a214739edb9a96931f9649faef
Unknown <d.t.nguyen@live.com>
2022-01-06 16:54:58 -0500

    replace hardcoded variables

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
+import "components/InterviewerListItem.scss"
+
 export default function InterviewerListItem(props) {
-  return (
+  return(
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src= { props.avatar }
+        alt={ props.name}
       />
-      Sylvia Palmer
+      { props.name }
     </li>
-  )}
+  )};
