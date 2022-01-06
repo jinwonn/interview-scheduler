af640bcfed4ac43b00b458cb145540b93900dc67
Unknown <d.t.nguyen@live.com>
2022-01-05 21:44:18 -0500

    fixed coditional wording for rendering remaining spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,24 @@ export default function DayListItem(props) {
       'day-list__item--selected': props.selected,
       'day-list__item--full': props.spots === 0
     })
+  
+  let formatSpots = (props) => {
+    let outputString = props.spots
+    if (props.spots === 0) outputString = "no" 
+
+    if (props.spots === 1) {
+      outputString += " spot remaining"
+    } else {
+      outputString += " spots remaining" 
+    }
+
+    return outputString
+  };
+
   return (
     <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className = "text--regular">{props.name}</h2>
-      <h3 className = "text--light">{props.spots} spots remaining</h3>
+      <h3 className = "text--light">{formatSpots(props)}</h3>
     </li>
   )
 }
