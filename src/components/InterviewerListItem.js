2bf38aafb7c924492d4b9526b388aacd8bd277bd
Unknown <d.t.nguyen@live.com>
2022-01-06 17:02:35 -0500

    classNames based on selected status

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,10 +1,16 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/InterviewerListItem.scss"
 
 export default function InterviewerListItem(props) {
+  let interviewerListItemClassName = classNames(
+    "interviewers__item", {
+      "interviewers__item--selected": props.selected
+    }
+  )
   return(
-    <li className="interviewers__item">
+    <li className={ interviewerListItemClassName } onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
         src= { props.avatar }
