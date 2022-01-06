4a1b61471d796d0ed6924f7d720504268ab350dd
Unknown <d.t.nguyen@live.com>
2022-01-06 17:38:14 -0500

    import InterviewersListItem component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,22 @@
 import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss"
 
 export default function InterviewerList(props) {
+  const parsedData = props.interviewers.map( interviewer => 
+      <InterviewerListItem 
+        key = {interviewer.id} 
+        id = {interviewer.id}
+        avatar = { interviewer.avatar }
+        name = { interviewer.name }
+      />
+  )
+    
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{ parsedData }</ul>
     </section>
   )
 }
