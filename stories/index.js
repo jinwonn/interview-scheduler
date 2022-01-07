f67815c5435f84c64ea77f9c3a247f20b46c7b3f
Unknown <d.t.nguyen@live.com>
2022-01-07 14:32:10 -0500

    props for Show component

--- stories/index.js
+++ stories/index.js
@@ -27,6 +27,12 @@ storiesOf("Button", module)
     </Button>
   ));
 
+  const interviewer = {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png"
+  };
+
   storiesOf("Appointment", module)
     .addParameters({
       backgrounds: [{ name: "white", value: "#fff", default: true}]
@@ -35,4 +41,11 @@ storiesOf("Button", module)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm"/>)
     .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
-    .add("Show", () => <Show />)
+    .add("Show", () => (
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
+      />
+    ))
