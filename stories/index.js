546a4a5715a864adc738d1d01412b61d4618f4ee
Unknown <d.t.nguyen@live.com>
2022-01-07 00:02:03 -0500

    update storyboard with new onChange prop keyword

--- stories/index.js
+++ stories/index.js
@@ -59,11 +59,11 @@ storiesOf("DayListItem", module)
       backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
     })
     .add("Monday", () => (
-      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+      <DayList days={days} day={"Monday"} onChange={action("setDay")} />
     ))
     .add("Tuesday", () => (
-      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+      <DayList days={days} day={"Tuesday"} onChange={action("setDay")} />
     ))
     .add("Wednesday", () => (
-        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+        <DayList days={days} day={"Wednesday"} onChange={action("setDay")} />
     ));
