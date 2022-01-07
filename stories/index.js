e702f4223bda326fc265acd922e7c000e4113a9d
Unknown <d.t.nguyen@live.com>
2022-01-07 15:56:44 -0500

    hard coded Form component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Show from "components/Appointment/Show.js"
 import Confirm from "components/Appointment/Confirm.js"
 import Status from "components/Appointment/Status.js"
 import Error from "components/Appointment/Error.js"
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -123,5 +124,21 @@ storiesOf("Button", module)
       <Error
         message="Could not delete appointment"
         onClose={action("onClose")}
+      />
+    ))
+    .add("Edit", () => (
+      <Form
+        student="Lydia Miller-Jones"
+        interviewer={3}
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />
+    ))
+    .add("Create", () => (
+      <Form
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
       />
     ))
