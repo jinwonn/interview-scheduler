cbe733f701efb007971aa8123db6f2e8d4d9e37e
Unknown <d.t.nguyen@live.com>
2022-01-07 15:10:19 -0500

    Error component

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import Empty from "components/Appointment/Empty.js"
 import Show from "components/Appointment/Show.js"
 import Confirm from "components/Appointment/Confirm.js"
 import Status from "components/Appointment/Status.js"
+import Error from "components/Appointment/Error.js"
 
 storiesOf("Button", module)
   .addParameters({
@@ -59,3 +60,9 @@ storiesOf("Button", module)
       />
     ))
     .add("Status", () => <Status message="Deleting"/>)
+    .add("Error", () => (
+      <Error
+        message="Could not delete appointment"
+        onClose={action("onClose")}
+      />
+    ))
