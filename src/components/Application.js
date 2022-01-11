ff867bd26d171e6370e2afbcab5e536aaca6f875
Unknown <d.t.nguyen@live.com>
2022-01-11 15:36:23 -0500

    removed hardcoded appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,56 +6,20 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
-    days: []
+    days: [],
+    appointments: {}
   })
 
   const setDay = day => setState({...state, day});
   const setDays = days => setState(prev => ({...prev, days}));
 
-  const parsedAppointments = appointments.map((appointment) => 
-    <Appointment key={appointment.id === appointments.length ? "last" : appointment.id} {...appointment} />
+  const dailyAppointments = [];
+
+  const parsedAppointments = dailyAppointments.map((appointment) => 
+    <Appointment key={appointment.id === dailyAppointments.length ? "last" : appointment.id} {...appointment} />
   )
 
   useEffect(() =>{
