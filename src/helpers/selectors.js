08ee1b4e761bd2d1d852e7e4f94470b5c0735c10
Unknown <d.t.nguyen@live.com>
2022-01-11 15:00:47 -0500

    selector to filter appointments for a specific day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  const days = state.days
+  let filteredDays = null
+  let appointmentsArray = null
+  let appointmentsForDay = []
+  
+  if (days.length > 0) filteredDays = days.filter(item => item.name === day)
+  
+  if (filteredDays && filteredDays.length > 0) appointmentsArray = filteredDays[0].appointments;
+  
+  if (appointmentsArray)  appointmentsForDay = appointmentsArray.map(appointment => state.appointments[appointment])
+  
+  return appointmentsForDay;
+}
