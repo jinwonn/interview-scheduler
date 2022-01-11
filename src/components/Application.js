36f8acad633739640142063d09bf1692323cc904
Unknown <d.t.nguyen@live.com>
2022-01-11 13:37:09 -0500

    Axios to get data from api and render in DaysList

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
@@ -46,28 +47,16 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([])
 
   const parsedAppointments = appointments.map((appointment) => 
     <Appointment key={appointment.id === appointments.length ? "last" : appointment.id} {...appointment} />
   )
+
+  useEffect(() =>{
+   axios.get("api/days").then(res => setDays(res.data))
+  },[])
+
   return (
     <main className="layout">
       <section className="sidebar">      
