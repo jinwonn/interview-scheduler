2ea00e1752afa87a35eedd3ffdfb0915f088d25e
Unknown <d.t.nguyen@live.com>
2022-01-12 15:47:45 -0500

    back property added

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,25 @@ import { useState } from "react"
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
 
   const transition = (initial) => {
     setMode(initial)
+    const newHistory = history
+    newHistory.push(initial)
+    setHistory(newHistory)
   };
 
-  return {mode, transition};
+  const back = () => {
+    if (history.length === 1) return;
+    const previousMode = history[history.length - 2]
+    const newHistory = history
+
+    newHistory.pop()
+    setHistory(newHistory)
+    
+    setMode(previousMode)
+  }
+
+  return {mode, transition, back};
 }
