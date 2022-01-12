96f180e6eb85f569fc8ccec2e2b03bd9761d40cd
Unknown <d.t.nguyen@live.com>
2022-01-12 16:11:27 -0500

    CREATE mode for Form rendering

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,11 +5,13 @@ import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE ="CREATE";
   
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -18,11 +20,19 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {mode === EMPTY && <Empty onAdd={()=> console.log("clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={()=> transition(CREATE)} />}
+      
       {mode === SHOW && (
         <Show 
           student={props.interview.student} 
           interviewer={props.interview.interviewer} 
+        />
+      )}
+
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={()=>back()} 
         />
       )} 
     </article>
