45a98751974ed8c7ab765bbdabc9b1d7be37d7c0
Unknown <d.t.nguyen@live.com>
2022-01-12 21:23:09 -0500

    fixed merge issue

--- src/components/Application.js
+++ src/components/Application.js
@@ -16,9 +16,7 @@ export default function Application(props) {
   })
 
   const setDay = day => setState({...state, day});
-
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const dailyInterviewers = getInterviewersForDay(state, state.day)
+  
   const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
@@ -36,6 +34,7 @@ export default function Application(props) {
   };
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day)
 
   const parsedAppointments = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview)
