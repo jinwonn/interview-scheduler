402a289d7f9dc7c1f29e8695158bdf5bd91bd16e
Daniel <d.t.nguyen@live.com>
2022-01-16 17:31:18 -0500

    websicket implementation for multiple users to receive updates

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -5,6 +5,7 @@ export default function useApplicationData () {
 
   const SET_DAY = "SET_DAY";
   const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const UPDATE_APPLICATION_DATA ="UPDATE_APPLICATION_DATA";
 
   const reducer = (state, action)  => {
     switch (action.type) {
@@ -12,6 +13,45 @@ export default function useApplicationData () {
         return { ...state, day: action.value }
       case SET_APPLICATION_DATA:
         return { ...state, ...action.value }
+      case UPDATE_APPLICATION_DATA:
+        const createAppointmentsObject = (id, interview) =>{
+          const appointment = {
+            ...state.appointments[id],
+            interview: interview ? { ...interview} : null
+          };
+        
+          return {
+            ...state.appointments,
+            [id]: appointment
+          };
+        }
+      
+        const createDaysArray = (appointments) => {
+          const day = state.day;
+          const days = state.days;
+          const dayObject = days.filter((item) => item.name === day)[0]
+          const index = days.indexOf(dayObject)
+        
+          const freeSpots = dayObject.appointments.reduce((acc,val) => {
+            if(!appointments[val].interview) acc++
+            return acc
+          },0)
+
+          const newDayObject = {
+            ...dayObject,
+            spots: freeSpots
+          }
+        
+          days.splice(index, 1, newDayObject);
+
+          return days;
+        }
+
+        const appointments = createAppointmentsObject(action.value.id, action.value.interview);
+        const days = createDaysArray(appointments);
+        const applicationData = { appointments, days }
+        
+        return { ...state, ...applicationData }
       default:
         throw new Error(
           `Tried to reduce with unsupported action type: ${action.type}`
@@ -27,7 +67,7 @@ export default function useApplicationData () {
     });
 
   const setDay = day => dispatch({type: SET_DAY, value: day});
-
+  
   useEffect(() =>{
     Promise.all([
       axios.get("api/days"),
@@ -36,60 +76,27 @@ export default function useApplicationData () {
     ]).then(all => {
       const [days, appointments, interviewers] = all
       const applicationData = 
-        { days: days.data, 
-          appointments: appointments.data, 
-          interviewers: interviewers.data
-        }
+      { days: days.data, 
+        appointments: appointments.data, 
+        interviewers: interviewers.data
+      }
 
       dispatch({ type: SET_APPLICATION_DATA, value: applicationData})
+      
+      const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+      webSocket.onmessage = function (event) {
+        const message = JSON.parse(event.data)
+        if(message.type === "SET_INTERVIEW") {
+          const applicationData = {id: message.id, interview: message.interview}
+          console.log(applicationData)
+          dispatch({ type: UPDATE_APPLICATION_DATA, value: applicationData})
+        }
+      }
     });
    },[])
-  
-  const createAppointmentsObject = (id, interview) =>{
-    const appointment = {
-      ...state.appointments[id],
-      interview: interview ? { ...interview} : null
-    };
-
-    return {
-      ...state.appointments,
-      [id]: appointment
-    };
-  }
-
-  const createDaysArray = (appointments) => {
-    const day = state.day;
-    const days = state.days;
-    const dayObject = days.filter((item) => item.name === day)[0]
-    const index = days.indexOf(dayObject)
-
-    const freeSpots = dayObject.appointments.reduce((acc,val) => {
-      if(!appointments[val].interview) acc++
-      return acc
-    },0)
-    
-    const newDayObject = {
-      ...dayObject,
-      spots: freeSpots
-    }
-
-    days.splice(index, 1, newDayObject);
-    
-    return days;
-  }
-
-  const setApplicationData = (id, interview) => {
-    const appointments = createAppointmentsObject(id, interview);
-    const days = createDaysArray(appointments);
-    const applicationData = { appointments, days }
-    dispatch({ type: SET_APPLICATION_DATA, value: applicationData})
-  };
 
   const handleResponse = (response, id, interview) => {
-    if (response.status ===204) {
-      setApplicationData(id, interview);
-    }
-
+    if (response.status ===204) return
     if (response.status === 500) {
       throw new Error('Status 500');
     }
@@ -98,14 +105,14 @@ export default function useApplicationData () {
   const bookInterview = (id, interview) => {
     return axios.put(`/api/appointments/${id}`, {interview})
     .then(response => {
-      handleResponse(response, id, interview);
+      handleResponse(response);
     })
   };
 
   const deleteInterview = (id) => {
     return axios.delete(`api/appointments/${id}`)
     .then(response =>{
-      handleResponse(response, id);
+      handleResponse(response);
     })
   };
 
